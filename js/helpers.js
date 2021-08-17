01d2c67df84b7c37060d7e8cf817472e40490b12
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-08-17 18:29:01 +0500

    created helpers.js

---
+++ js/helpers.js
@@ -0,0 +1,30 @@
+import { TIMEOUT_SEC } from './config.js';
+
+const timeout = function (s) {
+  return new Promise((_, reject) => {
+    setTimeout(function () {
+      reject(new Error(`Request took too long. Timeout after ${s} seconds`));
+    }, s * 1000);
+  });
+};
+
+export const AJAX = async function (url, uploadData = null) {
+  try {
+    const fetchPromise = uploadData
+      ? fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(uploadData),
+        })
+      : fetch(url);
+    const res = await Promise.race([fetchPromise, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+    if (!res.ok) throw new Error(`Please add a link`);
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+};
