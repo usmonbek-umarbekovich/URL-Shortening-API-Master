e4c4f78cfb6ffcef25832c7abad733aba98d64bf
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-08-17 18:29:21 +0500

    created model.js

---
+++ js/model.js
@@ -0,0 +1,34 @@
+import { API_URL } from './config.js';
+import { AJAX } from './helpers.js';
+
+export let links = [];
+
+const saveLink = function () {
+  localStorage.setItem('links', JSON.stringify(links));
+};
+
+const getSavedLinks = function () {
+  return JSON.parse(localStorage.getItem('links'));
+};
+
+export const shortenLink = async function (longLink) {
+  try {
+    const { result } = await AJAX(`${API_URL}/shorten?url=${longLink}`);
+    const { full_short_link, original_link } = result;
+    const newLink = { shortLink: full_short_link, originalLink: original_link };
+    links.push(newLink);
+    saveLink();
+    return newLink;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const init = function() {
+  const savedLink = getSavedLinks();
+  if (savedLink && savedLink.length > 0) {
+    links = savedLink;
+  }
+}
+
+init();
