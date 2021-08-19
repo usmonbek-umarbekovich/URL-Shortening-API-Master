f7c7b94f7ff3e6a9a1902d8d1c4999fd3a747ac6
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-08-19 23:44:14 +0500

    created module for dom elements

---
+++ js/elements.js
@@ -0,0 +1,20 @@
+export const navbar = document.querySelector('#navbar');
+export const hamburger = document.querySelector('.hamburger');
+export const linkContainer = document.querySelector('.link-container');
+
+export const heading = document.querySelector('.heading');
+export const subHeading = document.querySelector('.sub-heading');
+
+export const spinner = document.querySelector('.spinner');
+export const btnShorten = document.querySelector('.btn-shorten');
+export const shortenForm = document.querySelector('.shortening-form');
+export const inputField = document.querySelector('#raw-link');
+export const shortenResults = document.querySelector('.shorten-result');
+export const errorContainer = document.querySelector('.error-message');
+
+export const btnStart = document.querySelectorAll('.btn-start');
+export const lHeading = document.querySelector('.l-heading');
+export const headingDetail = document.querySelector('.heading-detail');
+export const cards = document.querySelectorAll('.card');
+export const cardContainer = document.querySelector('.stats-items');
+export const cardLine = document.querySelector('.stats-line');
