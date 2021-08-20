f033887cdaf6adbbd7f9b01bbbc2a84f7bafec01
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2021-08-20 18:27:21 +0500

    removed unused elements

--- js/elements.js
+++ js/elements.js
@@ -2,9 +2,6 @@ export const navbar = document.querySelector('#navbar');
 export const hamburger = document.querySelector('.hamburger');
 export const linkContainer = document.querySelector('.link-container');
 
-export const heading = document.querySelector('.heading');
-export const subHeading = document.querySelector('.sub-heading');
-
 export const spinner = document.querySelector('.spinner');
 export const btnShorten = document.querySelector('.btn-shorten');
 export const shortenForm = document.querySelector('.shortening-form');
@@ -13,8 +10,6 @@ export const shortenResults = document.querySelector('.shorten-result');
 export const errorContainer = document.querySelector('.error-message');
 
 export const btnStart = document.querySelectorAll('.btn-start');
-export const lHeading = document.querySelector('.l-heading');
-export const headingDetail = document.querySelector('.heading-detail');
 export const cards = document.querySelectorAll('.card');
 export const cardContainer = document.querySelector('.stats-items');
 export const cardLine = document.querySelector('.stats-line');
